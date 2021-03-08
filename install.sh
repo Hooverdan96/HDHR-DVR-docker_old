8f91b6419e9db331382a8d30fe99c89f5ff94222
Hooverdan96 <35113775+Hooverdan96@users.noreply.github.com>
2021-03-08 15:49:24 -0800

    Deprecating install.sh

--- install.sh
+++ install.sh
@@ -7,21 +7,21 @@
 # Version 1.1
 
 # Install working wget which preserves server timestamp
-apk update
-apk add wget
-apk add grep 
+# apk update
+# apk add wget
+# apk add grep 
 
 # Create default user and group
-addgroup -g 1000 hdhr
-adduser -HDG hdhr -u 1000 hdhr
+# addgroup -g 1000 hdhr
+# adduser -HDG hdhr -u 1000 hdhr
 
 # Create working directory
-mkdir -p /HDHomeRunDVR
+# mkdir -p /HDHomeRunDVR
 
 # Create volume mount points
-mkdir /dvrdata
-mkdir /dvrrec
+# mkdir /dvrdata
+# mkdir /dvrrec
 
 # Link to where the runtime script expects them
-ln -s /dvrdata /HDHomeRunDVR/data
-ln -s /dvrrec /HDHomeRunDVR/recordings
+# ln -s /dvrdata /HDHomeRunDVR/data
+# ln -s /dvrrec /HDHomeRunDVR/recordings
